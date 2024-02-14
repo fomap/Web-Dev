39a100f0bd58c07e256d98c715be4b4a71133dd8
Beka <anellbektassova@gmail.com>
2024-02-15 00:13:26 +0600

    documentation stuff

---
+++ lab3/js fundamentals/file.js
@@ -0,0 +1,119 @@
+// alert( 'I’m JavaScript!' );
+
+//1)
+// var admin;
+// var name;
+// name = 'John'
+// admin = name
+// alert(admin)
+
+//2)
+// let planet = 'Earth'
+// let user = 'Beka'
+
+// 3)
+// let name = "Ilya";
+// alert( `hello ${1}` ); // ?
+// alert( `hello ${"name"}` ); // ?
+// alert( `hello ${name}` ); // ?
+
+// 4)
+// let name = prompt("Whats ur name?");
+// alert(name)
+
+// 5)
+// let a = prompt("First number?", 1);
+// let b = prompt("Second number?", 2);
+
+// alert(Number(a) + Number(b)); // 12
+
+// 6)
+// let name = prompt("What is the “official” name of JavaScript?")
+
+// if (name == 'ECMAScript'){
+//     alert("Right")
+// }
+// else
+// {
+//     alert("You don’t know? ECMAScript!")  
+// }
+
+// 7)
+// var num = prompt("Enter number");
+
+// if(num > 0){
+//     alert(1)
+// }
+// else if (num < 0){
+//     alert(-1)
+// }
+// else{
+//     alert(0)
+// }
+
+// 8)
+// let result = (a + b < 4) ? 'Below' : 'Over';
+
+// 9)
+// var login = prompt("Enter login info");
+// if(login == "Admin"){
+//     var password = prompt("Enter password");
+
+//     if(password === "TheMaster"){
+//         alert("Welcome");
+//     }
+//     else if (password === null || pass === ''){
+//         alert("Canceled");
+//     }
+//     else{
+//         alert("Wrong Password");
+//     }
+// }
+// else if (login === null || login === ''){
+//     alert("Canceled");
+// }
+// else{
+//     alert("I dont know you");
+// }
+
+// 10)
+// for(var i = 2; i < 10; i = i + 2){
+//     alert(i);
+// }
+
+// 11)
+// var i = 0;
+// while(i < 3){
+//     alert(i);
+//     i++;
+// }
+
+// 12)
+// var flag = false
+
+// while(flag === false){
+//     var input = prompt("Enter number greater than 100")
+
+//     if(input > 100){
+//         flag = true
+//     }
+// }
+
+// 13)
+// var n = prompt("Enter number")
+
+
+// prime:
+// for(var i = 2; i < n; i++){
+    
+
+//     for(var j = 2; j < i; j++){
+//         if (i%j == 0){
+//         continue prime;
+//         }
+//     }
+
+//     alert(i);
+// }
+
+
