697cfc2aca6cb6512d55ac823ae9bd9017e456e5
Beka <anellbektassova@gmail.com>
2024-03-23 16:37:42 +0500

    initial commit

---
+++ lab5/lab5new/src/app/products.ts
@@ -0,0 +1,88 @@
+export interface Product {
+  image?: HTMLImageElement
+  name: string;
+  description: string;
+  rating: number;
+  link: string;
+}
+
+export const products = [
+  {
+    image:  "https://resources.cdn-kaspi.kz/img/m/p/h24/h7a/67278437548062.jpg?format=gallery-large",
+    name: 'Монитор Sanc M2442PH серый',
+    description: 'диагональ: 23.8 дюйм, разрешение: 1920x1080, макс. частота обновления кадров: 165.0 Гц,',
+    rating: 5,
+    link: "https://kaspi.kz/shop/p/sanc-m2442ph-seryi-108213498/?c=750000000",
+  },
+  {
+    image: "https://resources.cdn-kaspi.kz/img/m/p/h73/hd6/67881278603294.jpg?format=gallery-large" ,
+    name: "Книга Сатрапи: Персеполис" ,
+    description: "ISBN: 978-5-906331-96-0" , 
+    rating: 5 ,
+    link: "https://kaspi.kz/shop/p/satrapi-m-persepolis-108443966/?c=750000000",
+  },
+  {
+    image: "https://resources.cdn-kaspi.kz/img/m/p/h1c/h51/83950750695454.jpg?format=gallery-large" ,
+    name: "Мини-принтер Centechia Mini голубой",
+    description: "тип: портативный технология печати: струйная", 
+    rating: 5 ,
+    link: "https://kaspi.kz/shop/p/centechia-mini-goluboi-113459009/?c=750000000",
+    
+  },
+  {
+    image: "https://resources.cdn-kaspi.kz/img/m/p/he5/h4a/84701020356638.jpg?format=gallery-large" ,
+    name: "Коврик для мыши Inova Death Life",
+    description: "материал покрытия: ткань игровой: Да", 
+    rating: 5,
+    link: "https://kaspi.kz/shop/p/inova-death-life-115283473/?c=750000000",
+  },
+  {
+    image: "https://resources.cdn-kaspi.kz/img/m/p/h1a/hb4/64342870753310.jpg?format=gallery-large" ,
+    name: "Книга Кристи Агата.: Убийства по алфавиту",
+    description: "ISBN: 9785040996957", 
+    rating: 5 ,
+    link: "https://kaspi.kz/shop/p/kristi-a-ubiistva-po-alfavitu-101516844/?c=750000000",
+  },
+  {
+    image: "https://resources.cdn-kaspi.kz/img/m/p/h49/h36/64432195403806.jpg?format=gallery-large" ,
+    name: "Вафельница Proliss Pro-808 черный",
+    description: "материал корпуса: металл/пластик тип: вафельница", 
+    rating: 4.5 ,
+    link: "https://kaspi.kz/shop/p/vafel-nitsa-proliss-pro-808-chernyi-105935489/?c=750000000",
+  },
+  {
+    image: "https://resources.cdn-kaspi.kz/img/m/p/h25/hf0/63820113543198.jpg?format=gallery-large" ,
+    name: "Карта памяти Kingston SDCS2/128GB 128 Гб",
+    description: "тип: microSDXC объем памяти: 128 Гб", 
+    rating: 5 ,
+    link: "https://kaspi.kz/shop/p/kingston-sdcs2-128gb-128-gb-100081405/?c=750000000",
+  },
+  {
+    image: "https://resources.cdn-kaspi.kz/img/m/p/hb2/hc2/64119540187166.jpg?format=gallery-large" ,
+    name: "Мышь Logitech G102 Lightsync черный",
+    description: "тип сенсора: оптическая светодиодная", 
+    rating: 5,
+    link: "https://kaspi.kz/shop/p/logitech-g102-lightsync-chernyi-100956618/?c=750000000",
+  },
+  {
+    image: "https://resources.cdn-kaspi.kz/img/m/p/h9f/hcc/64026052984862.jpg?format=gallery-large" ,
+    name: "Фотопленка Instax Instax Mini EU 2 глянец 20 шт 20 шт",
+    description: "тип пленки: цветная количество кадров: 20", 
+    rating: 5,
+    link: "https://kaspi.kz/shop/p/instax-instax-mini-eu-2-gljanets-20-sht-20-sht-101116053/?c=750000000",
+  },
+  {
+    image: "https://resources.cdn-kaspi.kz/img/m/p/h02/h93/63945994633246.jpg?format=gallery-large" ,
+    name: "Игровой контроллер Sony PS5 DualSense белый",
+    description: "тип контроллера: геймпад тип подключения: беспроводной", 
+    rating: 5,
+    link: "https://kaspi.kz/shop/p/sony-ps5-dualsense-belyi-100752003/?c=750000000",
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
